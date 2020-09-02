986d9d57c052df111dc8add9655aaf66f453a3ef
long <long9ka@gmail.com>
2020-09-03 02:35:49 +0700

    update .gitignore

--- storage/framework/views/b19b7e89e5665f972f0094b6b4f4c661abf63b20.php
+++
@@ -1,8 +0,0 @@
-Name : <?php echo e($data['name']); ?><br>
-Gender : <?php echo e($data['sex']); ?><br>
-Phone : <?php echo e($data['phone']); ?><br>
-Email : <?php echo e($data['email']); ?><br>
-Message : <?php echo e($data['message']); ?><br>
-
-Curriculum Vitae:
-<img src="<?php echo e($message->embed(asset('public/tmp_image/'.$data['cv']))); ?>"><?php /**PATH C:\wamp64\www\SGSS\resources\views/frontEnd/job_apply_details.blade.php ENDPATH**/ ?>
